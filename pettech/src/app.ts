f07492fe08dc7da0f7c22ca317e5fab9a3288fe6
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-06-29 10:42:23 -0300

    global handler created

--- pettech/src/app.ts
+++ pettech/src/app.ts
@@ -1,29 +1,11 @@
 import fastify from 'fastify'
 import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from './http/controllers/user/routes'
-import { ZodError } from 'zod'
-import { env } from './env'
-import { ResourceNotFoundError } from './use-cases/errors/resource-not-found-error'
+import { globalErrorHandler } from './utils/global-error-handler'
 
 export const app = fastify()
 
 app.register(personRoutes)
 app.register(userRoutes)
 
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validation error', errors: error.format() })
-  }
-
-  if (error instanceof ResourceNotFoundError) {
-    return reply.status(404).send({ message: 'Resource not found' })
-  }
-
-  if (env.NODE_ENV === 'development') {
-    console.error(error)
-  }
-
-  return reply.status(500).send({ message: 'Internal server error' })
-})
+app.setErrorHandler(globalErrorHandler)
