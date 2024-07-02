12fac3dcb3b4368058b9bb5bd2e5ef3fdf6de839
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-07-01 22:49:02 -0300

    configuring typeorm to connect to the database

---
+++ pettech/src/entities/category.entity.ts
@@ -0,0 +1,22 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm'
+import { ICategory } from './models/category.interface'
+
+export class Category implements ICategory {
+  @PrimaryGeneratedColumn('increment', {
+    name: 'id',
+  })
+  id?: number | undefined
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+  })
+  name: string
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp without time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt?: Date | undefined
+}
