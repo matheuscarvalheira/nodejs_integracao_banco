a945985abbb74f829bb73794a39a5ca69972bf25
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-07-09 15:37:01 -0300

    applying error handling, pipe and validation with zod

--- stock_pettech_product/src/stock/services/stock.service.ts
+++ stock_pettech_product/src/stock/services/stock.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProductRepository } from '../repositories/product.repository';
 
 @Injectable()
@@ -10,7 +10,11 @@ export class StockService {
   }
 
   async getStock(productId: string) {
-    return this.productRepository.getStock(productId);
+    const product = await this.productRepository.getStock(productId);
+
+    if (!product) throw new NotFoundException('Product not found');
+
+    return product;
   }
 
   async createStock(product) {
