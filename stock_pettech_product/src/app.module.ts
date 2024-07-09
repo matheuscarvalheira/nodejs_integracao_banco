7738f9ee8b4904244da62a75def301d308990404
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-07-09 16:58:25 -0300

    replacing with environment variables

--- stock_pettech_product/src/app.module.ts
+++ stock_pettech_product/src/app.module.ts
@@ -4,14 +4,18 @@ import { AppService } from './app.service';
 import { StockModule } from './stock/stock.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/pettech'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     StockModule,
     JwtModule.register({
       global: true,
-      secret: 'mrrobot',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '10m' },
     }),
   ],
